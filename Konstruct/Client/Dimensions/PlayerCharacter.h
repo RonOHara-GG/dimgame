2e41093f0b8021bc28b723b8f673ad2165269d1a
bflassing <bflassing@0c57dbdb-4d19-7b8c-568b-3fe73d88484e>
2010-03-25 23:41:46 +0000

    Added PlayerClass class and implemented adding and removing a PlayerClass.

--- Konstruct/Client/Dimensions/PlayerCharacter.h
+++ Konstruct/Client/Dimensions/PlayerCharacter.h
@@ -3,6 +3,8 @@
 #include "Common/Utility/kpuVector.h"
 #include "Actor.h"
 #include  "Item.h"
+#include "PlayerClass.h"
+
 
 class kpgRenderer;
 class Skill;
@@ -18,89 +20,31 @@ public:
 	void Update(float fGameTime);
 	void Draw(kpgRenderer* pRenderer);
 
-	kpuVector GetLocation();
-	void SetLocation(const kpuVector& vNewLoc);
-
-	int GetLevel() { return m_iLevelBraw + m_iLevelArch + m_iLevelMark + m_iLevelMedi + m_iLevelOccu + m_iLevelPrie + m_iLevelRock + m_iLevelSword; }
+	int GetLevel();
 
 	void GainExp(int iExp); //Distributes exp over player's classes
 
-	enum PlayerClasses
-	{
-		eCL_Brawler,
-		eCL_Swordsman,
-		eCL_Archer,
-		eCL_Marksman,
-		eCL_Rocketeer,
-		eCL_Medic,
-		eCL_Priest,
-		eCL_Occultist,
-	};
+	bool AddNewClass(PlayerClass::Class eClass, float fExpPercent); //Adds a new class to the player, returns false if the class could not be added
+	float RemoveClass(PlayerClass::Class eClass); //Removes a class from the player and returns the amount of exp that class was getting
 
 protected:
 
+	void PlayerInit();
+
 	void UpdateMovement(float fGameTime); //Update the movement of the player
 
-	void LevelUp(PlayerCharacter::PlayerClasses eClass); //Handles distirbution of skill and attribute points when a class reaches a new level
+	void LevelUp(); //Handles distirbution of skill and attribute points when a class reaches a new level
 
 	//Leveling up stuff
-	int				m_iAttribPoints;
-
-	int				m_iLevelBraw;
-	int				m_iPointsBraw;
-	float			m_fCurrExpBraw;
-	float			m_fNeedExpBraw;	
-	float			m_fExpSplitBraw;
-
-	int				m_iLevelSword;
-	int				m_iPointsSword;
-	float			m_fCurrExpSword;
-	float			m_fNeedExpSword;	
-	float			m_fExpSplitSword;
-
-	int				m_iLevelArch;
-	int				m_iPointsArch;
-	float			m_fCurrExpArch;
-	float			m_fNeedExpArch;	
-	float			m_fExpSplitArch;
-
-	int				m_iLevelMark;
-	int				m_iPointsMark;
-	float			m_fCurrExpMark;
-	float			m_fNeedExpMark;
-	float			m_fExpSplitMark;
-
-	int				m_iLevelRock;
-	int				m_iPointsRock;
-	float			m_fCurrExpRock;
-	float			m_fNeedExpRock;	
-	float			m_fExpSplitRock;
-
-	int				m_iLevelMedi;
-	int				m_iPointsMedi;
-	float			m_fCurrExpMedi;
-	float			m_fNeedExpMedi;	
-	float			m_fExpSplitMedi;
-
-	int				m_iLevelPrie;
-	int				m_iPointsPrie;
-	float			m_fCurrExpPrie;
-	float			m_fNeedExpPrie;	
-	float			m_fExpSplitPrie;
-
-	int				m_iLevelOccu;
-	int				m_iPointsOccu;
-	float			m_fCurrExpOccu;
-	float			m_fNeedExpOccu;	
-	float			m_fExpSplitOccu;
-
+	int				m_iAttribPoints;	
 
 	//Inventory
 	Item			m_aInventory[INVENTORY_SIZE];
 
+	PlayerClass*	m_aClasses[8];
 	
 };
 
 #define ATTRIBUTE_POINTS_PER_LEVEL 5
-#define SKILL_POINTS_MULTIPLE 2
+
 #define EXP_EXPONENT 3.75
