a56bf030d94d69e53cecb056adbb666518fffc87
bflassing <bflassing@0c57dbdb-4d19-7b8c-568b-3fe73d88484e>
2010-03-25 04:58:38 +0000

    Added leveling up stuff and exp distribution to player class

--- Konstruct/Client/Dimensions/PlayerCharacter.cpp
+++ Konstruct/Client/Dimensions/PlayerCharacter.cpp
@@ -54,7 +54,174 @@ PlayerCharacter::~PlayerCharacter(void)
 
 
 
+void PlayerCharacter::GainExp(int iExp)
+{
+	if(m_iLevelArch > 0)
+	{
+		m_fCurrExpArch += iExp * m_fExpSplitArch;
 
+		if(m_fCurrExpArch >= m_fNeedExpArch)
+		{
+			m_fCurrExpArch -= m_fNeedExpArch;
+			LevelUp(eCL_Archer);
+			
+		}
+	}
+
+	if(m_iLevelBraw> 0)
+	{
+		m_fCurrExpBraw += iExp * m_fExpSplitBraw;
+
+		if(m_fCurrExpBraw >= m_fNeedExpBraw)
+		{
+			m_fCurrExpBraw -= m_fNeedExpBraw;
+
+			LevelUp(eCL_Brawler);
+		}
+	}
+
+	if(m_iLevelMark > 0)
+	{
+		m_fCurrExpMark += iExp * m_fExpSplitMark ;
+
+		if(m_fCurrExpMark  >= m_fNeedExpMark )
+		{
+			m_fCurrExpMark  -= m_fNeedExpMark ;
+
+			LevelUp(eCL_Marksman);
+		}
+	}
+
+	if(m_iLevelMedi > 0)
+	{
+		m_fCurrExpMedi += iExp * m_fExpSplitMedi;
+
+		if(m_fCurrExpMedi >= m_fNeedExpMedi)
+		{
+			m_fCurrExpMedi -= m_fNeedExpMedi;
+
+			LevelUp(eCL_Medic);
+		}
+	}
+
+	if(m_iLevelSword > 0)
+	{
+		m_fCurrExpSword += iExp * m_fExpSplitSword;
+
+		if(m_fCurrExpSword >= m_fNeedExpSword)
+		{
+			m_fCurrExpSword -= m_fNeedExpSword;
+
+			LevelUp(eCL_Swordsman);
+		}
+	}
+
+	if(m_iLevelPrie> 0)
+	{
+		m_fCurrExpPrie += iExp * m_fExpSplitPrie;
+
+		if(m_fCurrExpPrie >= m_fNeedExpPrie)
+		{
+			m_fCurrExpPrie -= m_fNeedExpPrie;
+
+			LevelUp(eCL_Priest);
+		}
+	}
+
+	if(m_iLevelRock> 0)
+	{
+		m_fCurrExpRock += iExp * m_fExpSplitRock;
+
+		if(m_fCurrExpRock >= m_fNeedExpRock)
+		{
+			m_fCurrExpRock -= m_fNeedExpRock;
+
+			LevelUp(eCL_Rocketeer);
+		}
+	}
+
+	if(m_iLevelOccu > 0)
+	{
+		m_fCurrExpOccu += iExp * m_fExpSplitOccu;
+
+		if(m_fCurrExpOccu >= m_fNeedExpOccu)
+		{
+			m_fCurrExpOccu -= m_fNeedExpOccu;
+
+			LevelUp(eCL_Occultist);
+		}
+	}
+
+
+
+
+}
+
+void PlayerCharacter::LevelUp(PlayerCharacter::PlayerClasses eClass)
+{
+	m_iAttribPoints += ATTRIBUTE_POINTS_PER_LEVEL;
+
+	switch(eClass)
+	{
+		case eCL_Brawler:
+			{
+				m_iLevelBraw++;
+				m_iPointsBraw += SKILL_POINTS_MULTIPLE * m_iLevelBraw;
+
+				break;
+			}
+		case eCL_Archer:
+			{
+				m_iLevelArch++;
+				m_iPointsArch += SKILL_POINTS_MULTIPLE * m_iLevelArch;
+
+				break;
+			}
+		case eCL_Marksman:
+			{
+				m_iLevelMark++;
+				m_iPointsMark += SKILL_POINTS_MULTIPLE * m_iLevelMark;
+
+				break;
+			}
+		case eCL_Medic:
+			{
+				m_iLevelMedi++;
+				m_iPointsMedi += SKILL_POINTS_MULTIPLE * m_iLevelMedi;
+				break;
+			}
+		case eCL_Occultist:
+			{
+				m_iLevelOccu++;
+				m_iPointsOccu += SKILL_POINTS_MULTIPLE * m_iLevelOccu;
+
+				break;
+			}
+		case eCL_Rocketeer:
+			{
+				m_iLevelRock++;
+				m_iPointsRock += SKILL_POINTS_MULTIPLE * m_iLevelRock;
+
+				break;
+			}
+		case eCL_Priest:
+			{
+				m_iLevelPrie++;
+				m_iPointsPrie += SKILL_POINTS_MULTIPLE * m_iLevelPrie;
+
+				break;
+			}
+		case eCL_Swordsman:
+			{
+				m_iLevelSword++;
+				m_iPointsSword += SKILL_POINTS_MULTIPLE * m_iLevelSword;
+
+				break;
+			}
+	}
+
+
+}
 
 void PlayerCharacter::Update(float fGameTime)
 {
--- Konstruct/Client/Dimensions/PlayerCharacter.h
+++ Konstruct/Client/Dimensions/PlayerCharacter.h
@@ -21,6 +21,10 @@ public:
 	kpuVector GetLocation();
 	void SetLocation(const kpuVector& vNewLoc);
 
+	int GetLevel() { return m_iLevelBraw + m_iLevelArch + m_iLevelMark + m_iLevelMedi + m_iLevelOccu + m_iLevelPrie + m_iLevelRock + m_iLevelSword; }
+
+	void GainExp(int iExp); //Distributes exp over player's classes
+
 	enum PlayerClasses
 	{
 		eCL_Brawler,
@@ -37,44 +41,62 @@ protected:
 
 	void UpdateMovement(float fGameTime); //Update the movement of the player
 
+	void LevelUp(PlayerCharacter::PlayerClasses eClass); //Handles distirbution of skill and attribute points when a class reaches a new level
+
 	//Leveling up stuff
+	int				m_iAttribPoints;
+
 	int				m_iLevelBraw;
+	int				m_iPointsBraw;
 	float			m_fCurrExpBraw;
 	float			m_fNeedExpBraw;	
+	float			m_fExpSplitBraw;
 
 	int				m_iLevelSword;
+	int				m_iPointsSword;
 	float			m_fCurrExpSword;
 	float			m_fNeedExpSword;	
+	float			m_fExpSplitSword;
 
 	int				m_iLevelArch;
+	int				m_iPointsArch;
 	float			m_fCurrExpArch;
 	float			m_fNeedExpArch;	
+	float			m_fExpSplitArch;
 
 	int				m_iLevelMark;
+	int				m_iPointsMark;
 	float			m_fCurrExpMark;
-	float			m_fNeedExpMark;	
+	float			m_fNeedExpMark;
+	float			m_fExpSplitMark;
 
 	int				m_iLevelRock;
+	int				m_iPointsRock;
 	float			m_fCurrExpRock;
 	float			m_fNeedExpRock;	
+	float			m_fExpSplitRock;
 
 	int				m_iLevelMedi;
+	int				m_iPointsMedi;
 	float			m_fCurrExpMedi;
 	float			m_fNeedExpMedi;	
+	float			m_fExpSplitMedi;
 
 	int				m_iLevelPrie;
+	int				m_iPointsPrie;
 	float			m_fCurrExpPrie;
 	float			m_fNeedExpPrie;	
+	float			m_fExpSplitPrie;
 
 	int				m_iLevelOccu;
+	int				m_iPointsOccu;
 	float			m_fCurrExpOccu;
 	float			m_fNeedExpOccu;	
+	float			m_fExpSplitOccu;
 
 
 	//Inventory
 	Item			m_aInventory[INVENTORY_SIZE];
-
-	//Class Levels
 
 	
 };
