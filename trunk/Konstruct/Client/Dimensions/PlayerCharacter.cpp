cac39b131146390408fea1e25a84f0d1a8383ce9
bflassing <bflassing@0c57dbdb-4d19-7b8c-568b-3fe73d88484e>
2010-03-25 23:41:46 +0000

    Added PlayerClass class and implemented adding and removing a PlayerClass.

--- trunk/Konstruct/Client/Dimensions/PlayerCharacter.cpp
+++ trunk/Konstruct/Client/Dimensions/PlayerCharacter.cpp
@@ -13,214 +13,80 @@ PlayerCharacter::PlayerCharacter(void):Actor()
 		m_pModel = 0;
 	}	
 
-	m_iLevelBraw = 0;
-	m_fCurrExpBraw = 0.0f;
-	m_fNeedExpBraw = 0.0f;	
+}
 
-	m_iLevelSword = 0;
-	m_fCurrExpSword = 0.0f;
-	m_fNeedExpSword = 0.0f;	
+void PlayerCharacter::PlayerInit()
+{
+	ZeroMemory(m_aClasses, sizeof(m_aClasses));
 
-	m_iLevelArch = 0;
-	m_fCurrExpArch = 0.0f;
-	m_fNeedExpArch = 0.0f;	
+	m_fSpeed = 1.3f;
 
-	m_iLevelMark = 0;
-	m_fCurrExpMark = 0.0f;
-	m_fNeedExpMark = 0.0f;	
-
-	m_iLevelRock = 0;
-	m_fCurrExpRock = 0.0f;
-	m_fNeedExpRock = 0.0f;	
-
-	m_iLevelMedi = 0;
-	m_fCurrExpMedi = 0.0f;
-	m_fNeedExpMedi = 0.0f;	
-
-	m_iLevelPrie = 0;
-	m_fCurrExpPrie = 0.0f;
-	m_fNeedExpPrie = 0.0f;	
-
-	m_iLevelOccu = 0;
-	m_fCurrExpOccu = 0.0f;
-	m_fNeedExpOccu = 0.0f;	
 }
 
 PlayerCharacter::~PlayerCharacter(void)
 {
 	if(m_pModel)
 		delete m_pModel;
+
+	if(m_aClasses)
+		delete[] m_aClasses;
 }
 
+bool PlayerCharacter::AddNewClass(PlayerClass::Class eClass, float fExpPercent)
+{	
+	if(m_aClasses[eClass])
+		return false;
 
+	//check exp split
+	float fTotal = fExpPercent;
+
+	for(int i = 0; i < NUMBER_OF_CLASSES; i++)
+	{
+		if(m_aClasses[i])
+		{
+			fTotal += m_aClasses[i]->GetExpSplit();
+		}
+		else
+			break;
+	}
+
+	if(fTotal != 1.0f)
+		return false;
+	
+	m_aClasses[eClass] = new PlayerClass(eClass, fExpPercent);	
+
+}
+
+float PlayerCharacter::RemoveClass(PlayerClass::Class eClass)
+{
+	float fExpSplit = m_aClasses[eClass]->GetExpSplit();
+
+	delete m_aClasses[eClass];
+
+	return fExpSplit;
+}
 
 void PlayerCharacter::GainExp(int iExp)
 {
-	if(m_iLevelArch > 0)
+	
+	for(int i = 0; i < NUMBER_OF_CLASSES; i++)
 	{
-		m_fCurrExpArch += iExp * m_fExpSplitArch;
-
-		if(m_fCurrExpArch >= m_fNeedExpArch)
+		if(m_aClasses[i])
 		{
-			m_fCurrExpArch -= m_fNeedExpArch;
-			LevelUp(eCL_Archer);
-			
+			if(m_aClasses[i]->GainExp(iExp))
+				LevelUp();
 		}
+		else
+			break;
 	}
-
-	if(m_iLevelBraw> 0)
-	{
-		m_fCurrExpBraw += iExp * m_fExpSplitBraw;
-
-		if(m_fCurrExpBraw >= m_fNeedExpBraw)
-		{
-			m_fCurrExpBraw -= m_fNeedExpBraw;
-
-			LevelUp(eCL_Brawler);
-		}
-	}
-
-	if(m_iLevelMark > 0)
-	{
-		m_fCurrExpMark += iExp * m_fExpSplitMark ;
-
-		if(m_fCurrExpMark  >= m_fNeedExpMark )
-		{
-			m_fCurrExpMark  -= m_fNeedExpMark ;
-
-			LevelUp(eCL_Marksman);
-		}
-	}
-
-	if(m_iLevelMedi > 0)
-	{
-		m_fCurrExpMedi += iExp * m_fExpSplitMedi;
-
-		if(m_fCurrExpMedi >= m_fNeedExpMedi)
-		{
-			m_fCurrExpMedi -= m_fNeedExpMedi;
-
-			LevelUp(eCL_Medic);
-		}
-	}
-
-	if(m_iLevelSword > 0)
-	{
-		m_fCurrExpSword += iExp * m_fExpSplitSword;
-
-		if(m_fCurrExpSword >= m_fNeedExpSword)
-		{
-			m_fCurrExpSword -= m_fNeedExpSword;
-
-			LevelUp(eCL_Swordsman);
-		}
-	}
-
-	if(m_iLevelPrie> 0)
-	{
-		m_fCurrExpPrie += iExp * m_fExpSplitPrie;
-
-		if(m_fCurrExpPrie >= m_fNeedExpPrie)
-		{
-			m_fCurrExpPrie -= m_fNeedExpPrie;
-
-			LevelUp(eCL_Priest);
-		}
-	}
-
-	if(m_iLevelRock> 0)
-	{
-		m_fCurrExpRock += iExp * m_fExpSplitRock;
-
-		if(m_fCurrExpRock >= m_fNeedExpRock)
-		{
-			m_fCurrExpRock -= m_fNeedExpRock;
-
-			LevelUp(eCL_Rocketeer);
-		}
-	}
-
-	if(m_iLevelOccu > 0)
-	{
-		m_fCurrExpOccu += iExp * m_fExpSplitOccu;
-
-		if(m_fCurrExpOccu >= m_fNeedExpOccu)
-		{
-			m_fCurrExpOccu -= m_fNeedExpOccu;
-
-			LevelUp(eCL_Occultist);
-		}
-	}
-
 
 
 
 }
 
-void PlayerCharacter::LevelUp(PlayerCharacter::PlayerClasses eClass)
+void PlayerCharacter::LevelUp()
 {
 	m_iAttribPoints += ATTRIBUTE_POINTS_PER_LEVEL;
-
-	switch(eClass)
-	{
-		case eCL_Brawler:
-			{
-				m_iLevelBraw++;
-				m_iPointsBraw += SKILL_POINTS_MULTIPLE * m_iLevelBraw;
-
-				break;
-			}
-		case eCL_Archer:
-			{
-				m_iLevelArch++;
-				m_iPointsArch += SKILL_POINTS_MULTIPLE * m_iLevelArch;
-
-				break;
-			}
-		case eCL_Marksman:
-			{
-				m_iLevelMark++;
-				m_iPointsMark += SKILL_POINTS_MULTIPLE * m_iLevelMark;
-
-				break;
-			}
-		case eCL_Medic:
-			{
-				m_iLevelMedi++;
-				m_iPointsMedi += SKILL_POINTS_MULTIPLE * m_iLevelMedi;
-				break;
-			}
-		case eCL_Occultist:
-			{
-				m_iLevelOccu++;
-				m_iPointsOccu += SKILL_POINTS_MULTIPLE * m_iLevelOccu;
-
-				break;
-			}
-		case eCL_Rocketeer:
-			{
-				m_iLevelRock++;
-				m_iPointsRock += SKILL_POINTS_MULTIPLE * m_iLevelRock;
-
-				break;
-			}
-		case eCL_Priest:
-			{
-				m_iLevelPrie++;
-				m_iPointsPrie += SKILL_POINTS_MULTIPLE * m_iLevelPrie;
-
-				break;
-			}
-		case eCL_Swordsman:
-			{
-				m_iLevelSword++;
-				m_iPointsSword += SKILL_POINTS_MULTIPLE * m_iLevelSword;
-
-				break;
-			}
-	}
-
-
 }
 
 void PlayerCharacter::Update(float fGameTime)
