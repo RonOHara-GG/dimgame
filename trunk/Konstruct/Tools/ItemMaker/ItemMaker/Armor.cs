8236214adf7ab365a3c6a314b2f3dbd8c1baeddf
bflassing <bflassing@0c57dbdb-4d19-7b8c-568b-3fe73d88484e>
2010-04-13 06:05:02 +0000

    Tool to make some items, currently only can make weapons

---
+++ trunk/Konstruct/Tools/ItemMaker/ItemMaker/Armor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ItemMaker
+{
+    class Armor:Equippable
+    {
+        public override void Load(System.IO.BinaryReader br)
+        {
+            base.Load(br);
+        }
+
+        public override void Save(System.IO.BinaryWriter bw)
+        {
+            base.Save(bw);
+        }
+    }
+}
---
+++ trunk/Konstruct/Tools/ItemMaker/ItemMaker/Weapon.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using System.Windows.Forms;
+
+namespace ItemMaker
+{
+    class Weapon: Equippable
+    {
+        protected float m_fRange;
+        protected float m_fDamage;
+        protected float m_fRecovery;
+        protected int m_iDamageType;
+
+        public override void Save(BinaryWriter bw)
+        {
+            base.Save(bw);
+
+            bw.Write(m_fRange);
+            bw.Write(m_fDamage);
+            bw.Write(m_fRecovery);
+            bw.Write(m_iDamageType);            
+        }
+
+        public override void Load(BinaryReader br)
+        {
+            base.Load(br);
+
+            m_fRange = br.ReadSingle();
+            m_fDamage = br.ReadSingle();
+            m_fRecovery = br.ReadSingle();
+            m_iDamageType = br.ReadInt32();
+        }
+
+        public override void SetData(ArrayList textBoxes)
+        {
+            base.SetData(textBoxes);
+            
+            m_fRange = float.Parse(((TextBox)textBoxes[19]).Text);
+            m_fDamage = float.Parse(((TextBox)textBoxes[20]).Text);
+            m_fRecovery = float.Parse(((TextBox)textBoxes[21]).Text);
+            m_iDamageType = int.Parse(((TextBox)textBoxes[22]).Text);
+        }
+
+        public override void SetFields(ArrayList textBoxes)
+        {
+            base.SetFields(textBoxes);
+
+            ((TextBox)textBoxes[19]).Text = m_fRange.ToString();
+            ((TextBox)textBoxes[20]).Text = m_fDamage.ToString();
+            ((TextBox)textBoxes[21]).Text = m_fRecovery.ToString();
+            ((TextBox)textBoxes[22]).Text = m_iDamageType.ToString();
+
+        }
+    }
+}
